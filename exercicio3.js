3943dcbcdb8aa9c57e5be535e05ddfd2f17a59ee
aureliovianadev <aureliovianadev@gmail.com>
2026-02-26 21:39:38 -0300

    ex3 Feito

--- exercicio3.js
+++ exercicio3.js
@@ -3,7 +3,7 @@
 // delegando as ações para eles. O HTML não deve ser alterado: continua usando carro.motorLigado, carro.radioLigado e carro.volume
 // (você pode usar getters no Carro que leem de this.motor e this.radio).
 
-class Carro {
+/*class Carro {
     constructor() {
         this.motorLigado = false;
         this.radioLigado = false;
@@ -21,4 +21,66 @@ class Carro {
     aumentarVolume() {
         this.volume++;
     }
+}*/
+
+class Motor {
+    constructor() {
+        this.ligado = false;
+    }
+
+    ligar() {
+        this.ligado = true;
+    }
 }
+
+
+class Radio {
+    constructor() {
+        this.ligado = false;
+        this.volume = 0;
+    }
+
+    ligar() {
+        this.ligado = true;
+    }
+
+    aumentarVolume() {
+        this.volume++;
+    }
+}
+
+
+class Carro {
+    constructor() {
+        this.motor = new Motor(); 
+        this.radio = new Radio(); 
+    }
+
+    
+    ligarMotor() {
+        this.motor.ligar();
+    }
+
+    ligarRadio() {
+        this.radio.ligar();
+    }
+
+    aumentarVolume() {
+        this.radio.aumentarVolume();
+    }
+
+   
+    get motorLigado() {
+        return this.motor.ligado;
+    }
+
+    get radioLigado() {
+        return this.radio.ligado;
+    }
+
+    get volume() {
+        return this.radio.volume;
+    }
+}
+
+
